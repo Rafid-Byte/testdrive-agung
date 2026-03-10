1ac021a84efe4bc33390f80d81d380033452ced0
Rafid-Byte <rafiddypo18@gmail.com>
2026-03-10 14:50:14 +0700

    Remove unused seeders: TestDriveBookingSeeder and PameranBookingSeeder from DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -10,8 +10,6 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             UserSeeder::class,
-            TestDriveBookingSeeder::class,
-            PameranBookingSeeder::class,
         ]);
     }
 }
